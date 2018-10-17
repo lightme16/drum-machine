735f38166bd8a8e35c12ea2a1f64d238eca1c808
Alexander Zveruk <sanyazveruk@gmail.com>
2018-10-17 23:58:22 +0300

    Added samples and binded to clicks

--- src/App.js
+++ src/App.js
@@ -1,29 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import closed_hat2 from './samples/closed_hat2.wav';
+import kick2 from './samples/kick2.wav';
+import kick3 from './samples/kick3.wav';
+import open_hat from './samples/open_hat.wav';
+import shkr1 from './samples/shkr1.wav';
+import shkr3 from './samples/shkr3.wav';
+import snore1 from './samples/snore1.wav';
+import snore2 from './samples/snore2.wav';
+import tamp from './samples/tamp.wav';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div id="drum-machine">
-          <div id="display">
-              </div>div className='drum-pad' id='Q'>
-                <audio src='#'></audio>
-              </div>
-              </div>div className='drum-pad' id='W'></div>
-              </div>div className='drum-pad' id='E'></div>
-              </div>div className='drum-pad' id='A'></div>
-              </div>div className='drum-pad' id='S'></div>
-              </div>div className='drum-pad' id='D'></div>
-              </div>div className='drum-pad' id='Z'></div>
-              </div>div className='drum-pad' id='X'></div>
-              </div>div className='drum-pad' id='C'></div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    playAudio = (ev) => {
+        let audio = ev.target.childNodes[1];
+        audio.play();
+    };
+
+    render() {
+        return (
+            <div className='App'>
+                <div id="drum-machine">
+                    <div id="display">
+                        <div className='drum-pad' onClick={this.playAudio}>
+                            Q
+                            <audio className='clip' id='Q' src={closed_hat2}/>
+                        </div>
+                        <div className='drum-pad' onClick={this.playAudio}>
+                            W
+                            <audio className='clip' id='W' src={kick2}/>
+                        </div>
+                        <div className='drum-pad' onClick={this.playAudio}>
+                            E
+                            <audio className='clip' id='E' src={kick3}/>
+                        </div>
+                        <div className='drum-pad' onClick={this.playAudio}>
+                            A
+                            <audio className='clip' id='A' src={open_hat}/>
+                        </div>
+                        <div className='drum-pad' onClick={this.playAudio}>
+                            S
+                            <audio className='clip' id='S' src={shkr1}/>
+                        </div>
+                        <div className='drum-pad' onClick={this.playAudio}>
+                            D
+                            <audio className='clip' id='D' src={shkr3}/>
+                        </div>
+                        <div className='drum-pad' onClick={this.playAudio}>
+                            Z
+                            <audio className='clip' id='Z' src={snore1}/>
+                        </div>
+                        <div className='drum-pad' onClick={this.playAudio}>
+                            X
+                            <audio className='clip' id='X' src={snore2}/>
+                        </div>
+                        <div className='drum-pad' onClick={this.playAudio}>
+                            C
+                            <audio className='clip' id='X' src={tamp}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
